0d4fa9593f81820c2fcc52bda8533656af08172a
Brian <westbrayo@gmail.com>
2025-04-22 03:09:32 +0200

    react and express installation

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use("/api/auth", authRoutes);
+
+//MongoDB connection
+console.log("🔍 MONGO_URI från env:", process.env.MONGO_URL);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error("DB connection error:", err));
